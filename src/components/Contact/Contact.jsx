9b82a0a54c2cfd6f433f02c281533c48bc2abd57
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2026-04-11 19:25:24 -0600

    Se agregó responsividad a la sección de información de contacto

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,31 @@
+import emailIcon from "../../assets/contact-icons/email.svg";
+import phoneIcon from "../../assets/contact-icons/phone.svg";
+
 function Contact() {
   return (
     <section className="contact section" id="contact">
       <h2 className="contact__title">Información de contacto</h2>
-      <p className="contact__text">
-        Si buscas un desarrollador web full-stack confiable y apasionado por la
-        innovación, no dudes en contactarme a través de mis redes sociales o
-        enviarme un correo a:
-      </p>
-      <a
-        href="mailto:zahidtejedaamaro.dev@gmail.com"
-        className="contact__email"
-      >
-        zahidtejedaamaro.dev@gmail.com
-      </a>
+
+      <ul className="contact__info-list">
+        <li>
+          <a
+            href="mailto:zahidtejedaamaro.dev@gmail.com"
+            className="contact__item"
+          >
+            <img src={emailIcon} alt="Email" className="contact__icon" />
+            <span className="contact__info">
+              zahidtejedaamaro.dev@gmail.com
+            </span>
+          </a>
+        </li>
+
+        <li>
+          <a href="tel:+529994994048" className="contact__item">
+            <img src={phoneIcon} alt="Teléfono" className="contact__icon" />
+            <span className="contact__info">+52 99 9499 4048</span>
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
